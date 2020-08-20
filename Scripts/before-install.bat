4ab711268ea88ffb7b433116213b52ab43d112ac
Davidu Jayapackiam <davidu.jayapackiam@aspiresys.com>
2020-08-20 15:07:53 +0530

    scripts modified

--- Scripts/before-install.bat
+++
@@ -1,8 +0,0 @@
-del "c:\inetpub\wwwroot\*.html" /s /f /q
-
-REM Install Internet Information Server (IIS).
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
-
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Set-NetFirewallRule -Name "WINRM-HTTP-In-TCP-PUBLIC" -RemoteAddress Any
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Enable-PSRemoting
